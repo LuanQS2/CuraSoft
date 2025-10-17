831829798bcf01b2c2b90ea654bbd1543791a7e5
LuanQS2 <165271965+LuanQS2@users.noreply.github.com>
2025-10-16 22:36:48 -0300

    exclusão de arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { DefaultLoginComponent } from './components/default-login/default-login.component';
 
 export const routes: Routes = [
   {
     path: "home",
     component: HomeComponent
   },
-
-  {
-    path: "login",
-    component: DefaultLoginComponent
-  }
 ];
