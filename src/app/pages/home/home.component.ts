fcf9f3e2c93d800962e69586b6a4d744d97eb6d9
LuanQS2 <165271965+LuanQS2@users.noreply.github.com>
2025-10-16 21:45:22 -0300

    feat: login layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from "../../layouts/home-layout/home-header/home-header.component";
+import { AdminFooterComponent } from "../../layouts/admin-layout/admin-footer/admin-footer.component";
+import { HomeFooterComponent } from "../../layouts/home-layout/home-footer/home-footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    HomeHeaderComponent,
+    HomeFooterComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DefaultLoginComponent } from "../../components/default-login/default-login.component";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    DefaultLoginComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
